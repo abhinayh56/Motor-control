51ca23b2fba127436017cd107ab4c5acaf0cef15
abhinayh56 <kumarabhinay53@gmail.com>
2024-01-18 21:26:21 +0530

    updated_gui2

--- py_interface/data_store/data_store_tx_rx.py
+++ py_interface/data_store/data_store_tx_rx.py
@@ -1,3 +1,16 @@
+# multithreading
+thread_1_freq = 1.0
+thread_2_freq = 1.0
+thread_3_freq = 1.0
+
+thread_1_flag = None
+thread_2_flag = None
+thread_3_flag = None
+
+thread_1_time_last = None
+thread_2_time_last = None
+thread_3_time_last = None
+
 # rx_pkt buffer
 rx_pkt = []
 
@@ -13,50 +26,3 @@ rx_motor_current = 0.0
 # tx data
 tx_v_percent = 0.0
 
-# gui falags
-start_stop_flag = False
-open_loop_control_flag = False
-closed_loop_control_flag = False
-switch_2_angle_feedback_flag = False
-set_velocity_control = False
-set_position_control = False
-
-# feedback
-dth_dt    = 0.0
-th        = 0.0
-alpha     = 0.0
-
-# setpoint
-th0      = 0.0
-dth0_dt  = 0.0
-
-# control system
-ctrl_position = None
-ctrl_velocity = None
-
-pid_pos_dt    = 1.0
-pid_pos_Kp    = 0.0
-pid_pos_Ki    = 0.0
-pid_pos_Kd    = 0.0
-pid_pos_fc    = 0.0
-pid_pos_u_max = 0.0
-
-pid_vel_dt    = 1.0
-pid_vel_Kp    = 0.0
-pid_vel_Ki    = 0.0
-pid_vel_Kd    = 0.0
-pid_vel_fc    = 0.0
-pid_vel_u_max = 0.0
-
-# multithreading
-thread_1_freq = 1.0
-thread_2_freq = 1.0
-thread_3_freq = 1.0
-
-thread_1_flag = None
-thread_2_flag = None
-thread_3_flag = None
-
-thread_1_time_last = None
-thread_2_time_last = None
-thread_3_time_last = None
